dfeb4c7d6f3eb4412ad4f467087e3dc466e167bc
Charles209 <xiaotian980204@gmail.com>
2020-11-02 15:44:35 +0800

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/myapplication/Pop.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+public class Pop extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwin);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.6),(int)(height*.3));
+//popup a window with property of popwin.xml
+
+
+    }
+}
+
